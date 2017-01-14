a82cd70fa4fb7833c1ec7caae7732c408b8539db
PY <codeflitting@gmail.com>
2017-01-14 22:39:10 +0800

    use django-hosts package

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# --coding:utf-8--
+import sys
+from django.conf import settings
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+
+class IndexView(ListView):
+    template_name = "blog/index.html"
+
+    def get_queryset(self):
+        pass
