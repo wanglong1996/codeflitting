d1640790bf6b33b88ff9062b89ead062eb2e17cc
PY <codeflitting@gmail.com>
2017-01-19 00:20:07 +0800

    add markdown tools

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^tag/(?P<tag_id>\d+)$', views.IndexView.as_view(), name='tag'),
     url(r'^category/(?P<category_id>\d+)$', views.IndexView.as_view(), name='category'),
-    url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='article'),
+    # url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='article'),
+    url(r'^markdown/', views.MarkdownView.as_view(), name='markdown'),
 ]
